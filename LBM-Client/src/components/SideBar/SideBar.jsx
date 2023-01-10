1d33031b5fbdf50f74ea939121a2328764994797
agustin panizza <agustin_panizza@hotmail.com>
2023-01-09 21:31:35 -0300

    fix css

--- LBM-Client/src/components/SideBar/SideBar.jsx
+++ LBM-Client/src/components/SideBar/SideBar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./SideBar.scss";
 
 function SideBar() {
-  return <div>SideBar</div>;
+  return (
+    <div className="Container">
+      <div></div>
+    </div>
+  );
 }
 
 export default SideBar;
