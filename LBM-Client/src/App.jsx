0b7d27740c0d0d918e0b0eb418e5d703c38fe000
agustin panizza <agustin_panizza@hotmail.com>
2023-01-09 21:16:37 -0300

    react-app

--- LBM-Client/src/App.jsx
+++ LBM-Client/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import SideBar from "./components/SideBar/SideBar";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SideBar />
+      <Routes></Routes>
+    </div>
+  );
 }
 
 export default App;
