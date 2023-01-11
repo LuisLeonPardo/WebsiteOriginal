a27a9ea078b440f4ba844a44950980e0b0b00569
Lucas <luqasmagra@gmail.com>
2023-01-11 17:24:03 -0300

    FT: Responsive Home Section

--- LBM-Client/src/components/Landing/Home/Home.jsx
+++ LBM-Client/src/components/Landing/Home/Home.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import launchapp from "./assets/launchapp.svg";
 import NavBar from "../NavBar/NavBar";
 import "./Home.scss";
 
 export default function Home() {
   return (
-    <main className="main_items">
+    <main className="home_items">
       <NavBar />
-      <div>asddddddddddddddddddddddddd</div>
-      <div></div>
+      <section className="home_section">
+        <div className="section_text">
+          <h1>
+            Borrow Assets, <span>Create Opportunities</span> and Earn Interest
+          </h1>
+          <p>
+            DeFi protocol with a fluid and inclusive economy; at you service, to
+            help you on a daily basis
+          </p>
+        </div>
+        <div className="section_links">
+          <a className="launchapp">
+            <span>Launch App</span> <img src={launchapp} />
+          </a>
+          <a className="token">Add token libertum</a>
+        </div>
+      </section>
     </main>
   );
 }
