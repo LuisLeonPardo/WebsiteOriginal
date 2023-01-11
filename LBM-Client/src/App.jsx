c06dc49e1a5ff8d5c09f1fcfc163cdb863a9d9a3
Lucas <luqasmagra@gmail.com>
2023-01-10 21:17:19 -0300

    Refactor App.jsx con creacion de rutas

--- LBM-Client/src/App.jsx
+++ LBM-Client/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-import SideBar from "./components/SideBar/SideBar";
-import NavBar from "./components/Landing/NavBar/NavBar";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
+// import Dashboard from "./components/Dashboards/Dashboard";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      {/*<SideBar /> */}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
