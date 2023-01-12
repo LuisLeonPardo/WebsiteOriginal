184cf36d98017c0563213af255d7979265a7a56e
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-01-12 20:38:09 -0300

    carpetas de realestate y arreglo del BrowserRouter

--- LBM-Client/src/App.jsx
+++ LBM-Client/src/App.jsx
@@ -1,19 +1,18 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Landing from "./components/Landing/Landing";
+import { Route, Routes } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
 // import Dashboard from "./components/Dashboards/Dashboard";
-import "./App.css";
+import './App.css';
+import RealEstates from './components/RealEstates';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/realestate" element={<RealEstates />} />
+				</Routes>
+			</div>
+	);
 }
 
 export default App;
