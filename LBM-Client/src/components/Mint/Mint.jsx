7f99b9c4bc407aea851ec7a5ac8ee3f1dad357c8
Ladislao Godocik <ladislaogodocik13@gmail.com>
2023-01-12 15:41:22 -0300

    Estados terminados

--- LBM-Client/src/components/Mint/Mint.jsx
+++ LBM-Client/src/components/Mint/Mint.jsx
@@ -1,20 +1,45 @@
 import React from "react";
+import { useState } from "react";
 import "./Mint.scss";
 
 function Mint() {
+  const [first, setfirst] = useState("mint");
+
+  function changeStateMint() {
+    setfirst("mint");
+    console.log(first);
+  }
+
+  function changeStateRepay() {
+    setfirst("repay");
+    console.log(first);
+  }
+
   return (
     <div className="mint-container">
       <div className="title-mint">
         <h1>Mint/Repay LUSD</h1>
-        <div className='mint-repay'>
-          <h3>Mint LUSD</h3>
-          <h3>Repay LUSD</h3>
+        <div className="mint-repay">
+          <h3 onClick={changeStateMint}>Mint LUSD</h3>
+          <h3 onClick={changeStateRepay}>Repay LUSD</h3>
         </div>
       </div>
-      <p>
-        To mint LUSD with the Libertum Protocol, you need to enable it first
-      </p>
-      <h2>Confirm</h2>
+      {first == "mint" ? (
+        <div>
+          <p>
+            To mint LUSD with the Libertum Protocol, you need to enable it first
+          </p>
+          <h2>Confirm</h2>
+        </div>
+      ) : (
+        <div>
+          <p>
+            To repay LUSD with the Libertum Protocol, you need to enable it
+            first
+          </p>
+          <h2>Confirm</h2>
+        </div>
+      )}
     </div>
   );
 }
