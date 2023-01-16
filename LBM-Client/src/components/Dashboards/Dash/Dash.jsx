9a69377d0ca893c8f166efc45d6839b1ea95e9c8
Camilo <churtadoh92@gmail.com>
2023-01-16 10:17:18 -0500

    Se ubica la dash

--- LBM-Client/src/components/Dashboards/Dash/Dash.jsx
+++ LBM-Client/src/components/Dashboards/Dash/Dash.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import Slider from "../Slider/Slider";
 import VertSlider from "../VertSlider/VertSlider";
 import Mint from "../Mint/Mint";
-import UserInfo from "../UserInfo/UserInfo"
-import FinancialMarket from "../FinancialMarket/FinancialMarket"
+import UserInfo from "../UserInfo/UserInfo";
+import FinancialMarket from "../FinancialMarket/FinancialMarket";
+import "./Dash.scss";
 
 function Dash() {
 
 
   return (
-    <div>
-      <Slider />
-      <VertSlider />
-      <Mint />
-      <UserInfo/>
-      <FinancialMarket/>
+    <div className="general">
+      <div className="dash">
+      <div className="columnOne">
+        <UserInfo/>
+        <FinancialMarket/>
+      </div>
+      <div className="columnOne">
+        <Slider />
+        <VertSlider />
+        <Mint />
+      </div>
+      </div>
     </div>
   );
 }
