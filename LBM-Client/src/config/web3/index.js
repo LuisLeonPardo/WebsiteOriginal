e8dd7b705420f2606b725416aa10c30078e89a71
Agustin Panizza <agustin_panizza@hotmail.com>
2023-01-17 23:55:21 -0300

    Rainbowkit eliminado

---
+++ LBM-Client/src/config/web3/index.js
@@ -0,0 +1,13 @@
+import Web3 from "web3"
+import {InjectedConnector} from '@web3-react/injected-connector'
+const connector = InjectedConnector({
+    supportedChainIds:[
+        592
+    ]
+})
+
+const getLibrary = (provider) => {
+    return new Web3(provider);
+}
+
+export (getLibrary);
