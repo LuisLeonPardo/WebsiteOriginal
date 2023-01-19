e7d73f4acf49cd0cb80b642b27f77106dff0ec75
Lucas <luqasmagra@gmail.com>
2023-01-19 12:31:02 -0300

    Add animations on scroll & fix responsive general

--- LBM-Client/src/components/Landing/Home/Home.jsx
+++ LBM-Client/src/components/Landing/Home/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Tada from "react-reveal/Tada";
+import Fade from "react-reveal/Fade";
+
 import launchapp from "./assets/launchapp.svg";
 import NavBar from "../NavBar/NavBar";
 import "./Home.scss";
@@ -7,7 +10,9 @@ import "./Home.scss";
 export default function Home() {
   return (
     <main className="home_items">
-      <NavBar />
+      <Fade top>
+        <NavBar />
+      </Fade>
       <section className="home_section">
         <div>
           <div className="home_text">
@@ -20,14 +25,16 @@ export default function Home() {
             </p>
           </div>
           <div className="home_links">
-            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
-              <a href="/dashboard" className="launchapp">
-                <span>Launch App</span> <img src={launchapp} />
-              </a>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
-              <a className="token">Add token libertum</a>
-            </motion.div>
+            <Tada>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
+                <a href="/dashboard" className="launchapp">
+                  <span>Launch App</span> <img src={launchapp} />
+                </a>
+              </motion.div>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
+                <a className="token">Add token libertum</a>
+              </motion.div>
+            </Tada>
           </div>
         </div>
       </section>
