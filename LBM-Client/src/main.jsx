17c4d6adf47cdf4a3bfd464e0e6ca40b6d7df12f
Agustin Panizza <50965316+Agus1510@users.noreply.github.com>
2023-01-20 12:12:13 -0400

    Update main.jsx

--- LBM-Client/src/main.jsx
+++ LBM-Client/src/main.jsx
@@ -10,32 +10,6 @@ import { Web3ReactProvider } from "@web3-react/core";
 import { getLibrary } from "./config/web3";
 //wallet imports
 
-const AstarNetworkChain = {
-  id: 592,
-  name: "Astar",
-  network: "Astar Network",
-  iconUrl: "./icons/astar-logo.png",
-  iconBackground: "#fff",
-  nativeCurrency: {
-    decimals: 18,
-    name: "ASTAR",
-    symbol: "ASTR",
-  },
-  rpcUrls: {
-    default: {
-      http: ["https://evm.astar.network", "https://astar.public.blastapi.io"],
-    },
-  },
-  blockExplorers: {
-    default: { name: "Subscan", url: "https://astar.subscan.io" },
-    etherscan: {
-      name: "PolkadotJs",
-      url: "https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Fastar.api.onfinality.io%2Fpublic-ws#/explorer",
-    },
-  },
-  testnet: false,
-};
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
