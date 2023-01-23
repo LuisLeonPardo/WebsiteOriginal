171d9b30bcefee0fc064af80e7bb3c4c3e2cda8e
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-01-23 10:46:18 -0300

    merge a main

--- LBM-Client/src/components/WarningBuilding/index.jsx
+++ LBM-Client/src/components/WarningBuilding/index.jsx
@@ -3,17 +3,17 @@ import './index.scss';
 function WarningBuilding() {
 	return (
 		<div className="bg-warning">
-			<div className='warning'>
+			<div className="warning">
 				<div className="bandas">
-					<div className='banda inclinacion banda__negra'></div>
-					<div className='banda inclinacion banda__amarilla'></div>
-					<div className='banda inclinacion banda__negra'></div>
+					<div className="banda inclinacion banda__negra"></div>
+					<div className="banda inclinacion banda__amarilla"></div>
+					<div className="banda inclinacion banda__negra"></div>
 				</div>
-				<h1 className='titulo'>Website under construction</h1>
+				<h1 className="titulo">Website under construction</h1>
 				<div className="bandas">
-					<div className='banda inclinacion__invertida banda__negra'></div>
-					<div className='banda inclinacion__invertida banda__amarilla'></div>
-					<div className='banda inclinacion__invertida banda__negra'></div>
+					<div className="banda inclinacion__invertida banda__negra"></div>
+					<div className="banda inclinacion__invertida banda__amarilla"></div>
+					<div className="banda inclinacion__invertida banda__negra"></div>
 				</div>
 			</div>
 		</div>
