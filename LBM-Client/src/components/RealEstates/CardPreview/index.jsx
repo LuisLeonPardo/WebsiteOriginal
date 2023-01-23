1a5164780fd7da4c11e72cf64df3f34394705327
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-01-23 19:18:51 -0300

    codigo documentado

--- LBM-Client/src/components/RealEstates/CardPreview/index.jsx
+++ LBM-Client/src/components/RealEstates/CardPreview/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './index.scss';
 function CardPreview({ image, number, fiveColumn }) {
+	//En este componente de renderiza la imagen y el numero de la land pasadas por props
+	//recibe por props image (imagen de la land), number (numero de la land) y fiveColumn que es un booleando
+	//Aclaracion: de momento todo esto es un maquetado, posiblemente mas adelante sea necesario hacer modificaciones para las variables dinamicas que podamos recibir
 	return (
 		<div
+			//Aqui utilizamos fiveColumn para el manejo de estilos a traves de un ternario
 			className={`${
 				fiveColumn ? 'cardPreviewFiveColumns' : 'cardPreviewFourColumns'
 			}`}
@@ -18,6 +22,7 @@ function CardPreview({ image, number, fiveColumn }) {
 								<th>Highest bid</th>
 							</tr>
 							<tr>
+								{/* Posiblemente mas adelante esta 2 <td> sean variables dinamicas */}
 								<td>200 LUSD</td>
 								<td>No bids Yet</td>
 							</tr>
