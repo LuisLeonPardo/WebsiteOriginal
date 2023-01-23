c4afcc2a174bddfaf685be58817793673ee718d4
Agustin Panizza <50965316+Agus1510@users.noreply.github.com>
2023-01-23 18:16:13 -0400

    Update SideBarMobile.jsx

--- LBM-Client/src/components/SideBar/mobile/SideBarMobile.jsx
+++ LBM-Client/src/components/SideBar/mobile/SideBarMobile.jsx
@@ -4,13 +4,16 @@ import style2 from "../SideBar.module.scss";
 import SideBar from "../SideBar";
 import { IoListOutline } from "react-icons/io5";
 import CustomConnectButtom from "../CustomConnectButtom";
+import { NavLink } from "react-router-dom";
 
 function SideBarMobile() {
   const [open, setOpen] = useState(false);
   return (
     <div className={style.Container}>
       <div className={style.BarMobile}>
-        <img src="./icons/Logo.svg" alt="logo" />
+        <NavLink to={"./"}>
+          <img src="./icons/Logo.svg" alt="logo" />
+        </NavLink>
         <div className={style.wallet}>
           <CustomConnectButtom container={style2.OpenContainer} />
         </div>
