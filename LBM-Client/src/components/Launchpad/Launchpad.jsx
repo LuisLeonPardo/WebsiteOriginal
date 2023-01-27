8d00b1d87aaf542fa87aca053a14d6c45c3d234c
Lucas <luqasmagra@gmail.com>
2023-01-27 12:13:10 -0300

    Add progress bar and refactor folders with files

--- LBM-Client/src/components/Launchpad/Launchpad.jsx
+++ LBM-Client/src/components/Launchpad/Launchpad.jsx
@@ -1,50 +1,48 @@
 import "./Launchpad.scss";
-import CardLP from "./CardLP";
-import info from "./info.json"
+import CardLP from "./CardLP/CardLP";
+import info from "./info.json";
 import { useState } from "react";
-import Frame from "./Frame.svg"
+import Frame from "./Frame.svg";
 
-export default function Launchpad () {
+export default function Launchpad() {
+  const [type, setType] = useState("ICO");
 
-    const [type, setType] = useState("ICO")
+  const [state, setState] = useState("ended");
 
-    const [state, setState] = useState('ended')
+  const [drop, setDrop] = useState({
+    ICO: true,
+    IREO: false,
+  });
 
-    const[drop, setDrop] =useState(
-        {
-            ICO: true,
-            IREO: false
-        }
-    )
+  const handleState = (e) => {
+    setState(e.target.name);
+  };
 
-    const handleState = (e) => {
-        setState(e.target.name)
-    }
+  const handleType = (e) => {
+    setType(e.target.id);
+    let drop = {
+      ICO: false,
+      IREO: false,
+    };
+    setDrop({
+      ...drop,
+      [e.target.id]: true,
+    });
+  };
 
-    const handleType = (e) => {
-        setType(e.target.id)
-        let drop = {
-            ICO: false,
-            IREO: false
-        }
-        setDrop({
-            ...drop,
-            [e.target.id]: true
-        })
-        
-    }
+  const handleDrop = (e) => {
+    e.stopPropagation();
+    setDrop({
+      ICO: true,
+      IREO: true,
+    });
+  };
 
-    const handleDrop = (e) => {
-        e.stopPropagation()
-        setDrop({
-            ICO: true,
-            IREO: true
-        })
-    }
+  const filter = info.data.filter(
+    (el) => el.state === state && el.type == type
+  );
 
-    const filter = info.data.filter(el => el.state ===state &&  el.type == type)
-
-    const itemsRow = 3
+  const itemsRow = 3;
 
   const grid = filter.map(
     (
@@ -76,7 +74,6 @@ export default function Launchpad () {
     )
   );
 
-
   const layout = [];
 
   for (let i = 0; i < grid.length; i++) {
@@ -93,40 +90,58 @@ export default function Launchpad () {
     );
   }
 
-    return (
-        <div className="launchpad">
-            <div className="mainPort">
-                <div className="header">
-                    <div className="title">
-                        <h1>Launchpad</h1>
-                    </div>
-                    <div className="interact">
-                        <button name="upcoming" onClick={handleState} className={(state==='upcoming')? 'buttonOn': 'buttonOff'}>
-                            Upcoming
-                        </button>
-                        <button name='ended' onClick={handleState} className={(state==='ended')? 'buttonOn': 'buttonOff'}>
-                            Ended
-                        </button>
-                        <button className="refresh"></button>
-                        <input className="search" placeholder="Search by NFT's"></input>
-                        <div className={(type==='ICO')? 'selectOn': 'selectOff'}>
-                            {drop.ICO && <div id='ICO' onClick={handleType} className={(type==='ICO')? 'dropDownOn': 'dropDownOff'}>
-                                <div style={{width:'24px'}}/>
-                                ICO
-                                <img src={Frame} onClick={handleDrop}></img>
-                            </div>}
-                            {drop.IREO && <div id='IREO' onClick={handleType} className={(type==='IREO')? 'dropDownOn': 'dropDownOff'}>
-                                <div style={{width:'24px'}}/>
-                                IREO
-                                <img src={Frame} onClick={handleDrop}></img>
-                            </div>}
-                        </div>
-                    </div>
+  return (
+    <div className="launchpad">
+      <div className="mainPort">
+        <div className="header">
+          <div className="title">
+            <h1>Launchpad</h1>
+          </div>
+          <div className="interact">
+            <button
+              name="upcoming"
+              onClick={handleState}
+              className={state === "upcoming" ? "buttonOn" : "buttonOff"}
+            >
+              Upcoming
+            </button>
+            <button
+              name="ended"
+              onClick={handleState}
+              className={state === "ended" ? "buttonOn" : "buttonOff"}
+            >
+              Ended
+            </button>
+            <button className="refresh"></button>
+            <input className="search" placeholder="Search by NFT's"></input>
+            <div className={type === "ICO" ? "selectOn" : "selectOff"}>
+              {drop.ICO && (
+                <div
+                  id="ICO"
+                  onClick={handleType}
+                  className={type === "ICO" ? "dropDownOn" : "dropDownOff"}
+                >
+                  <div style={{ width: "24px" }} />
+                  ICO
+                  <img src={Frame} onClick={handleDrop}></img>
                 </div>
-                <div className="body">
-                    {layout}
+              )}
+              {drop.IREO && (
+                <div
+                  id="IREO"
+                  onClick={handleType}
+                  className={type === "IREO" ? "dropDownOn" : "dropDownOff"}
+                >
+                  <div style={{ width: "24px" }} />
+                  IREO
+                  <img src={Frame} onClick={handleDrop}></img>
                 </div>
+              )}
             </div>
+          </div>
+        </div>
+        <div className="body">{layout}</div>
+      </div>
     </div>
-  )
+  );
 }
