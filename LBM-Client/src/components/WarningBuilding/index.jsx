e0c81da4fa2e14bfdf3f10d2547fd803d88a47aa
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-01-27 14:34:19 -0300

    modal de aviso

--- LBM-Client/src/components/WarningBuilding/index.jsx
+++ LBM-Client/src/components/WarningBuilding/index.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import './index.scss';
-function WarningBuilding() {
+import { BsInfoCircle } from 'react-icons/bs';
+
+function WarningBuilding({ isOpen, closeModal }) {
 	return (
-		<div className="bg-warning">
-			<div className="warning">
-				<div className="bandas">
-					<div className="banda inclinacion banda__negra"></div>
-					<div className="banda inclinacion banda__amarilla"></div>
-					<div className="banda inclinacion banda__negra"></div>
+		<article
+			className={`modalBuilding ${isOpen ? null : 'is-close'}`}
+			onClick={() => closeModal()}
+		>
+			<div
+				className={`modalContainerBuilding ${isOpen ? null : 'is-close'}`}
+				onClick={(e) => e.stopPropagation()}
+			>
+				<div className='info'>
+					<BsInfoCircle className="icon" />
 				</div>
-				<h1 className="titulo">Website under construction</h1>
-				<div className="bandas">
-					<div className="banda inclinacion__invertida banda__negra"></div>
-					<div className="banda inclinacion__invertida banda__amarilla"></div>
-					<div className="banda inclinacion__invertida banda__negra"></div>
-				</div>
+				<h1 className="modalContainerBuilding__h1">
+					Website under construction
+				</h1>
+				<p className="modalContainerBuilding__p">
+					Thank you for visiting us, our website is currently under
+					construction. We are still working on improving it, what you are
+					seeing at the moment is just a preview, but you can still explore some
+					of our available sections.
+				</p>
+				<button onClick={() => closeModal()} className="information button__modal">More information</button>
+				<button onClick={() => closeModal()} className="understood button__modal">Undertood</button>
 			</div>
-		</div>
+		</article>
 	);
 }
 
