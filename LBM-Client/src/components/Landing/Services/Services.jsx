3f48a32f795f710ada0f0e8406ba35282eb1510d
Angel-1001 <126737816+Angel-1001@users.noreply.github.com>
2023-03-02 07:40:13 +0400

    Update Services.jsx

--- LBM-Client/src/components/Landing/Services/Services.jsx
+++ LBM-Client/src/components/Landing/Services/Services.jsx
@@ -14,12 +14,12 @@ export default function Services() {
           <div className="services_text">
             <h2>Libertum Services</h2>
             <p>
-              Libertum services were divided into 3 categories and will be
-              developed in 3 different times according to the avaze project. In
-              the first place we have the DeFi 1.0 loans with the plus of
-              minting stablecoins, in the second we will have the real estate
-              marketplace and finally encourage other projects to incur in
-              offering loan services with real collaterals
+              Libertum Project services are divided into 3 categories and are
+              developed in 3 different stages. In
+              the first stage we have the DeFi 1.0 loans with the addition of
+              minting stablecoins, in the second stage we have the real estate
+              marketplace which allows for tokenization of property, and finally we encourage other projects to take part in
+              offering loan services with real estate property collateral
             </p>
           </div>
         </Slide>
@@ -28,7 +28,7 @@ export default function Services() {
             <div className="services-quad_item">
               <img src={loans} />
               <h3>Loans</h3>
-              <p>The ability to passively earn through DeFi 2.0 loans</p>
+              <p>The ability to passively earn interest through DeFi 2.0 loans</p>
               <div></div>
             </div>
           </Slide>
@@ -39,8 +39,8 @@ export default function Services() {
                 Marketplace <br></br>Real Estate
               </h3>
               <p>
-                One of the first real estate markets where you can buy, sell and
-                invest in crypto
+                One of the first real estate markets where you can buy, sell, trade and
+                invest in crypto or fiat-currency with real estate collatoral
               </p>
               <div></div>
             </div>
@@ -50,8 +50,8 @@ export default function Services() {
               <img src={launchpad} />
               <h3>Launchpad Crypto</h3>
               <p>
-                Libertum promotes ventures and startups that want adventures in
-                the DeFi world and need an immediate Liquidity market
+                Libertum Project invites real estate owners, syndicates, individuals and startups that want a solid experience in
+                the DeFi world are looking for liquidity
               </p>
               <div></div>
             </div>
