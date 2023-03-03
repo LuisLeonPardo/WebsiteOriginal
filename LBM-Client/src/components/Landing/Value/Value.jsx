820dc1e7ad4a3aa99ca32065a58c77972bc0ec2b
Angel-1001 <126737816+Angel-1001@users.noreply.github.com>
2023-03-03 08:17:29 +0400

    Update Value.jsx

--- LBM-Client/src/components/Landing/Value/Value.jsx
+++ LBM-Client/src/components/Landing/Value/Value.jsx
@@ -18,7 +18,7 @@ export default function Value() {
             <div className="value-text_item">
               <h2>Real Estate & Technology</h2>
               <p>
-                Libertum Project allows for everyone to take part in the global US$369 Trillion real estate market <br></br><br></br> 
+                Libertum Project allows for everyone to take part in the global US$ 369 Trillion real estate market <br></br><br></br> 
                 Join us as we disrupt the real estate market and revolutionize the future of property investment and ownership <br></br><br></br>
                 We break down the barriers to make it simple, affordable and accessible for everyone to be able to realize the 
                 benefits of investing in real estate <br></br><br></br>
@@ -28,7 +28,7 @@ export default function Value() {
                 easily bought, sold or traded; reducing entry barriers and equally providing liquidity <br></br><br></br>
                 We empower our community with the knowledge, tools and DeFi solutions to 
                 benefit from real estate investment opportunities that have previously only been available to institutional and 
-                private high-net worth investors
+                private high net worth investors
               </p>
             </div>
             <div className="value-text_item">
@@ -36,7 +36,7 @@ export default function Value() {
               <p>
                 Is to be the gateway to real estate opportunities for everyone, anywhere, and at anytime with the aim of
                 democratizing the property investment market through tokenization, by lowering the high barriers of entry, increase liquidity for property owners AND investors, 
-                lower investment costs, simplify the process to invest and open up opportunities for everyone never seen before.
+                lower investment costs, simplify the process to invest and open up opportunities for everyone never seen before
               </p>
             </div>
           </div>
