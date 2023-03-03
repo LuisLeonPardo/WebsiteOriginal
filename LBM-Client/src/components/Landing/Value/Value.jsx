185ace873907744bf9fa610663b4c55782e09970
Angel-1001 <126737816+Angel-1001@users.noreply.github.com>
2023-03-03 08:05:37 +0400

    Update Value.jsx

--- LBM-Client/src/components/Landing/Value/Value.jsx
+++ LBM-Client/src/components/Landing/Value/Value.jsx
@@ -18,14 +18,14 @@ export default function Value() {
             <div className="value-text_item">
               <h2>Creativity & Technology</h2>
               <p>
-                Libertum Project allows for everyone to take part in the global US$ 369 Trillion real estate market<br></br><br></br> 
+                Libertum Project allows for everyone to take part in the global US$369 Trillion real estate market<br></br><br></br> 
                 Join us as we disrupt the real estate market and revolutionize the future of property investment and ownership.<br></br><br></br>
                 We break down the barriers to make it simple, affordable and accessible for everyone to be able to realize the 
                 benefits of investing in real estate.<br></br><br></br>
                 Libertum Project creates a fundamental shift in the future of real estate investment and ownership, we empower 
                 individuals to start building a property portfolio that earns a passive income.<br></br><br></br>
                 Powered by the $LBM Token, our blockchain based platform enables fractional ownership of properties which can be 
-                easily bought, sold or traded; reducing entry barriers and equally providing liquidity.<br></br> <br></br>
+                easily bought, sold or traded; reducing entry barriers and equally providing liquidity.<br></br><br></br>
                 We empower our community with the knowledge, tools and DeFi solutions to 
                 benefit from real estate investment opportunities that have previously only been available to institutional and 
                 private high-net worth investors.
