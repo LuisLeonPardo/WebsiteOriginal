1f686c3a2047dc45f6f078312d5fb1179eef877a
Angel-1001 <126737816+Angel-1001@users.noreply.github.com>
2023-03-03 05:46:41 +0400

    Update Protocol.jsx

--- LBM-Client/src/components/Landing/Protocol/Protocol.jsx
+++ LBM-Client/src/components/Landing/Protocol/Protocol.jsx
@@ -6,9 +6,9 @@ import arrow from "./assets/arrow.svg";
 import "./Protocol.scss";
 
 const firstParagraph =
-  "Real estate is the single biggest asset class that may be tokenized. Total real estate is worth around $326 trillion today and only 7% of this is available to retail investors. However, more than 80% of people believe real estate to be a good investment.";
+  "Real estate is the single biggest asset class that may be tokenized. Global real estate market size is worth around $369 trillion today and only 7% of this is available to retail investors. However, more than 80% of people believe real estate to be a good investment.";
 const secondParagraph =
-  "The Libertum Project democratizes real estate investment market by allowing each and everyone to invest in tokenized property with crypto or fiat-currency. With tokenization, real estate assets can be represented as tokens on the blockchain that will become as efficient and inexpensive to issue, transfer and trade as other cryptocurrencies.";
+  "The Libertum Project democratizes the real estate investment market by allowing each and everyone to invest in tokenized property with crypto or fiat-currency. With tokenization, real estate assets can be represented as tokens on the blockchain that will become as efficient and inexpensive to issue, transfer and trade as other cryptocurrencies.";
 export default function Protocol() {
   const [index, setIndex] = useState(1);
   const [paragraph, setParagraph] = useState(firstParagraph);
@@ -37,7 +37,7 @@ export default function Protocol() {
         <Slide direction={"right"} triggerOnce={"true"}>
           <div className="protocol_text">
             <h2>
-              What is the Libertum <br></br>Project?
+              The Libertum <br></br>Project?
             </h2>
             <div className="protocol_slide">
               <p>{paragraph}</p>
