c2e8973ccf4a53e304fd6cc02e249eaffd161d47
Angel-1001 <126737816+Angel-1001@users.noreply.github.com>
2023-03-03 06:33:50 +0400

    Update Value.jsx

--- LBM-Client/src/components/Landing/Value/Value.jsx
+++ LBM-Client/src/components/Landing/Value/Value.jsx
@@ -18,14 +18,14 @@ export default function Value() {
             <div className="value-text_item">
               <h2>Creativity & Technology</h2>
               <p>
-                Libertum Project allows for everyone to take part in the global US$ 326 Trillion real estate market, 
+                Libertum Project allows for everyone to take part in the global US$ 369 Trillion real estate market, 
                 Join us as we disrupt the real estate market and revolutionize the future of property investment and ownership.
                 We break down the barriers to make it simple, affordable and accessible for everyone to be able to realize the 
-                benefits of investing in real estate.
+                benefits of investing in real estate.<br></br>
                 Libertum Project creates a fundamental shift in the future of real estate investment and ownership, we empower 
-                individuals to start building a property portfolio that earns a passive income.
+                individuals to start building a property portfolio that earns a passive income.<br></br>
                 Powered by the $LBM Token, our blockchain based platform enables fractional ownership of properties which can be 
-                easily bought, sold or traded; reducing entry barriers and equally providing liquidity.
+                easily bought, sold or traded; reducing entry barriers and equally providing liquidity.<br></br>
                 We empower our community with the knowledge, tools and DeFi solutions to 
                 benefit from real estate investment opportunities that have previously only been available to institutional and 
                 private high-net worth investors.
@@ -35,8 +35,8 @@ export default function Value() {
               <h2>Our true Value</h2>
               <p>
                 Is to be the gateway to real estate opportunities for everyone, anywhere, and at anytime with the aim of
-                democratizing the property investment market, by lowering the high barriers of entry, increase liquidity, 
-                lower investment costs, decrease time to invest and open up opportunities never seen before.
+                democratizing the property investment market, by lowering the high barriers of entry, increase liquidity for property owners AND investors, 
+                lower investment costs, simplify the process to invest and open up opportunities for everyone never seen before.
               </p>
             </div>
           </div>
