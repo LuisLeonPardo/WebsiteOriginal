b850c25ed4fd1248d1e4f12dc77552f8598ea2c6
LOLE81 <martinezpm@gmail.com>
2023-03-07 23:34:36 -0300

    creates MarketPlace folder, Details component, adds link in App to visualize it /marketplace/details

--- LBM-Client/src/App.jsx
+++ LBM-Client/src/App.jsx
@@ -1,8 +1,8 @@
-import Landing from './components/Landing/Landing';
-import NotFound from './components/NotFound/NotFound';
+import Landing from "./components/Landing/Landing";
+import NotFound from "./components/NotFound/NotFound";
 // import Dashboard from "./components/Dashboards/Dashboard";
-import './App.css';
-import SideBar from './components/SideBar/SideBar';
+import "./App.css";
+import SideBar from "./components/SideBar/SideBar";
 import {
   Route,
   Routes,
@@ -24,26 +24,27 @@ import SideBarMobile from "./components/SideBar/mobile/SideBarMobile";
 import CoomingSoon from "./components/ComingSoon/ComingSoon";
 import Launchpad from "./components/Launchpad/Launchpad";
 import ProductPage from "./components/Launchpad/ProductPage/ProductPage";
-import GovernanceDetails from './components/Governance/GovernanceDetails/GovernanceDetails'
+import GovernanceDetails from "./components/Governance/GovernanceDetails/GovernanceDetails";
+import Details from "./components/MarketPlace/Details";
 function App() {
-	const [stateModal, setStateModal] = useState(false);
-	const { selectedIcon, walletPopUp } = useSelector(
-		(state) => state.reducerCompleto
-	);
-	const AppLayout = () => (
-		<>
-			<div className="bgImage">
-				<img src="./icons/Background.svg" />
-			</div>
+  const [stateModal, setStateModal] = useState(false);
+  const { selectedIcon, walletPopUp } = useSelector(
+    (state) => state.reducerCompleto
+  );
+  const AppLayout = () => (
+    <>
+      <div className="bgImage">
+        <img src="./icons/Background.svg" />
+      </div>
 
-			<Modals state={stateModal} setStateModal={setStateModal} />
+      <Modals state={stateModal} setStateModal={setStateModal} />
 
-			<div className="SideBar">
-				<SideBarMobile />
-			</div>
-			<Outlet />
-		</>
-	);
+      <div className="SideBar">
+        <SideBarMobile />
+      </div>
+      <Outlet />
+    </>
+  );
 
   return (
     <div className="App">
@@ -79,6 +80,14 @@ function App() {
             element={
               <div className="Page">
                 <RealEstates />
+              </div>
+            }
+          />
+          <Route
+            path={"/marketplace/details"}
+            element={
+              <div className="Page" style={{ flexDirection: "column" }}>
+                <Details />
               </div>
             }
           />
