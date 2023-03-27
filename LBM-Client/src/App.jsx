7f0625f0fc2cfdbff4805d44309c58acdd0b83c4
LOLE81 <martinezpm@gmail.com>
2023-03-27 14:00:56 -0300

    updates MarketPlace

--- LBM-Client/src/App.jsx
+++ LBM-Client/src/App.jsx
@@ -25,7 +25,7 @@ import CoomingSoon from "./components/ComingSoon/ComingSoon";
 import Launchpad from "./components/Launchpad/Launchpad";
 import ProductPage from "./components/Launchpad/ProductPage/ProductPage";
 import GovernanceDetails from "./components/Governance/GovernanceDetails/GovernanceDetails";
-import Marketplace from "./components/Marketplace/Marketplace";
+import Marketplace from "./components/MarketPlace/Marketplace";
 import Details from "./components/MarketPlace/Details/Details";
 import NavbarMarket from "./components/Landing/NavBar/NavbarMarket/NavbarMarket";
 function App() {
