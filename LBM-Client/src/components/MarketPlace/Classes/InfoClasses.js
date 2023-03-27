6503ee6f4e4fa696d8dbbfa4747011d80dea45a1
Agustin Panizza <agustin_panizza@hotmail.com>
2023-03-27 15:06:55 -0300

    fix

---
+++ LBM-Client/src/components/MarketPlace/Classes/InfoClasses.js
@@ -0,0 +1,134 @@
+import Adapted from './assets/Adapted.svg'
+import AmazingPools from './assets/AmazingPools.svg'
+import Popular from './assets/Popular.svg'
+import Novelty from './assets/Novelty.svg'
+import Cottage from './assets/Cottage.svg'
+import IncredibleViews from './assets/IncredibleViews.svg'
+import Castle from './assets/Castle.svg'
+import Domes from './assets/Domes.svg'
+import Tropical from './assets/Tropical.svg'
+import Mansions from './assets/Mansions.svg'
+import Department from './assets/Department.svg'
+import BeachFront from './assets/BeachFront.svg'
+import Rural from './assets/Rural.svg'
+import Luxury from './assets/Luxury.svg'
+import Particular from './assets/Particular.svg'
+import TinyHouses from './assets/TinyHouses.svg'
+import Alpine from './assets/Alpine.svg'
+import Impressive from './assets/Impressive.svg'
+import Underground from './assets/Underground.svg'
+import Vineyards from './assets/Vineyards.svg'
+import Design from './assets/Design.svg'
+import Floating from './assets/Floating.svg'
+import Islands from './assets/Islands.svg'
+import Golf from './assets/Golf.svg'
+import Lake from './assets/Lake.svg'
+import Farms from './assets/Farms.svg'
+import Isolated from './assets/Isolated.svg'
+import Lakefronts from './assets/Lakefronts.svg'
+import Stables from './assets/Stables.svg'
+import Towers from './assets/Towers.svg'
+import Historic from './assets/Historic.svg'
+
+const InfoClasses =
+    [
+        {
+            'name': 'Amazing Pools',
+            'img': AmazingPools
+        },
+        {
+            'name': 'Popular',
+            'img': Popular
+        },
+        {
+            'name': 'Novelty',
+            'img': Novelty
+        }, {
+            'name': 'Cottage',
+            'img': Cottage
+        }, {
+            'name': 'Incredible Views',
+            'img': IncredibleViews
+        }, {
+            'name': 'castles',
+            'img': Castle
+        }, {
+            'name': 'beach front',
+            'img': BeachFront
+        }, {
+            'name': 'Department',
+            'img': Department
+        }, {
+            'name': 'mansions',
+            'img': Mansions
+        }, {
+            'name': 'Tropical',
+            'img': Tropical
+        }, {
+            'name': 'domes',
+            'img': Domes
+        }, {
+            'name': 'impressive',
+            'img': Impressive
+        }, {
+            'name': 'alpine houses',
+            'img': Alpine
+        }, {
+            'name': 'tiny houses',
+            'img': TinyHouses
+        }, {
+            'name': 'underground houses',
+            'img': Underground
+        }, {
+            'name': 'Rural houses',
+            'img': Rural
+        }, {
+            'name': 'Particular houses',
+            'img': Particular
+        }, {
+            'name': 'Luxury',
+            'img': Luxury
+        }, {
+            'name': 'islands',
+            'img': Islands
+        }, {
+            'name': 'Floating houses',
+            'img': Floating
+        }, {
+            'name': 'Design',
+            'img': Design
+        }, {
+            'name': 'vineyards',
+            'img': Vineyards
+        }, {
+            'name': 'lakefront',
+            'img': Lakefronts
+        }, {
+            'name': 'Isolated',
+            'img': Isolated
+        }, {
+            'name': 'farms',
+            'img': Farms
+        }, {
+            'name': 'Lake',
+            'img': Lake
+        }, {
+            'name': 'Golf',
+            'img': Golf
+        }, {
+            'name': 'historic houses',
+            'img': Historic
+        }, {
+            'name': 'stables',
+            'img': Stables
+        }, {
+            'name': 'towers',
+            'img': Towers
+        }, {
+            'name': 'adapted',
+            'img': Adapted
+        },
+    ]
+
+
+export default InfoClasses
