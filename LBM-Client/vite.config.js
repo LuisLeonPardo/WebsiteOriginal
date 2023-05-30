214b77b2da5abd11a7cdcecfd2484d0e8b17c18f
LOLE81 <martinezpm@gmail.com>
2023-05-30 13:17:54 -0300

    adds configuration in vite.config.js

--- LBM-Client/vite.config.js
+++ LBM-Client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "app/javascript/App/"),
+      timers: "rollup-plugin-node-polyfills/polyfills/timers",
+    },
+  },
   plugins: [react()],
-})
+});
