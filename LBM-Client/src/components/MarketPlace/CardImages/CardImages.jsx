3289967e7db7f03dcc088cc31fcc97ff67dde8f5
Belen-Cufre <belen_cufre@hotmail.com>
2023-05-31 19:57:15 -0300

    Rebranding MarketPlace

---
+++ LBM-Client/src/components/MarketPlace/CardImages/CardImages.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "./CardImages.scss";
+import HeartLine from "./assets/HeartLine.svg";
+import Heart from "./assets/Heart.svg";
+
+import image1 from "../assets/Image2.png";
+import image2 from "../assets/Image3.png";
+import image3 from "../assets/Image4.png";
+import image4 from "../assets/Image5.png";
+
+const images = [image1, image2, image3, image4];
+
+function CardImages({ img }) {
+  const [favorite, setFavorite] = useState(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  const handleClickFavorite = () => {
+    setFavorite(!favorite);
+  };
+
+  const handleImageChange = (index) => {
+    setSelectedImageIndex(index);
+  };
+
+  const handleTouchStart = (event) => {
+    setTouchStartX(event.touches[0].clientX);
+  };
+
+  const handleTouchEnd = (event) => {
+    const touchEndX = event.changedTouches[0].clientX;
+    const touchDeltaX = touchEndX - touchStartX;
+
+    if (touchDeltaX > 50) {
+      // Deslizar hacia la izquierda
+      const newIndex = Math.max(selectedImageIndex - 1, 0);
+      setSelectedImageIndex(newIndex);
+    } else if (touchDeltaX < -50) {
+      // Deslizar hacia la derecha
+      const newIndex = Math.min(selectedImageIndex + 1, images.length - 1);
+      setSelectedImageIndex(newIndex);
+    }
+  };
+
+  return (
+    <div className="marketplace-card-image">
+      {favorite ? (
+        <img src={Heart} className="heart-icon" onClick={handleClickFavorite} />
+      ) : (
+        <img
+          src={HeartLine}
+          className="heart-icon"
+          onClick={handleClickFavorite}
+        />
+      )}
+      <div className="menu-nav">
+        {images?.length > 0 &&
+          images?.map((image, index) => (
+            <div
+              key={index}
+              className={`custom-button ${
+                selectedImageIndex === index ? "active" : ""
+              }`}
+              onClick={() => handleImageChange(index)}
+              id={`btn${index + 1}`}
+            />
+          ))}
+      </div>
+
+      <div
+        className="container-slider"
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <ul className="slider">
+          {images?.map((image, index) => (
+            <li
+              key={index}
+              id={`slide${index + 1}`}
+              className={selectedImageIndex === index ? "active" : ""}
+            >
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default CardImages;
--- LBM-Client/src/components/MarketPlace/Marketplace.jsx
+++ LBM-Client/src/components/MarketPlace/Marketplace.jsx
@@ -4,6 +4,10 @@ import "./Marketplace.scss";
 //import image from "./assets/image.png";
 import ModalFilter from "./ModalFilter/ModalFilter";
 import Classes from "./Classes/Classes";
+import image from "./assets/Image2.png";
+import CardImages from "./CardImages/CardImages";
+import Footer from "../RealEstates/Footer/Footer";
+// import Slide from "./Slide";
 
 function Marketplace() {
   return (
@@ -30,7 +34,7 @@ function Marketplace() {
           {[...Array(20)].map(() => {
             return (
               <div className="marketplace-card">
-                {/* <img src={image} /> */}
+                <CardImages />
                 <div className="marketplace-card-details">
                   <div className="marketplace-card-text">
                     <h1>Groveland, California</h1>
@@ -48,6 +52,7 @@ function Marketplace() {
           })}
         </div>
       </div>
+      <Footer/>
     </div>
   );
 }
